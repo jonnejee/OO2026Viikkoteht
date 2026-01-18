184998edbf18a692b0aaf3c321dbf52b850674c1
jonnejee <jonne.karjalainen123@gmail.com>
2026-01-18 18:39:22 +0200

    lisataan puuttuva game.h

---
+++ Game.h
@@ -0,0 +1,19 @@
+#ifndef GAME_H
+#define GAME_H
+
+class Game
+{
+private:
+    int maxNum;
+    int salaNum;
+    int arvaus;
+    int yritykset;
+
+public:
+    Game(int max);
+    ~Game();
+    void play();
+    void printResult();
+};
+
+#endif // GAME_H
